e51c16a366388b3b48fb4cb14847ce439747e3c9
MicroApt <MicroApt@DESKTOP-3QL9K4N>
2022-12-28 19:16:41 +0530

    testing build103

---
+++ Nwemavan/src/test/java/mavan.java
@@ -0,0 +1,13 @@
+import javax.sql.rowset.WebRowSet;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class mavan {
+	@Test
+	public void test()
+	{
+		
+	}
+}
